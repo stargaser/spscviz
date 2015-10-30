fix sign of background in stretch

spscinspector.py
@@ -70,7 +70,7 @@ class SourceInspectCamera(PanZoomCamera):
                int(self.rect.left):int(self.rect.right)]
         imin, imax = np.nanpercentile(imsect, [5.0, 99.0])
         #cmin = -0.01 + 1.2*self.sources['background'][self.sources.index==self.index].values[0]
-        smax = 1.2*self.sources['fluxtml'][self.sources.index==self.index].values[0]/1000.0 - imin
+        smax = 1.2*self.sources['fluxtml'][self.sources.index==self.index].values[0]/1000.0 + imin
         #print(imin,imax,smax)
         self.image.set_data(bytescale(self.img_data,
                      cmin=imin, cmax=smax))